2310083fe9e5e88b985b299f96a83e2a01653263
Razod <mortonrainy0178@gmail.com>
2024-08-12 23:38:00 +0530

    edit

--- test/test.py
+++ test/test.py
@@ -8,19 +8,30 @@
 # f.close()
 # q.close()
 
-file_with_content = open("myfile.txt", "r+")
-line = file_with_content.readline()
-a_wali_lines = []
-while line:
-    location = file_with_content.tell()
-    if "a" in line or "A" in line:
-        file_with_content.seek(0)
-        file_with_content.write("")
-        a_wali_lines.append(line)
-    line = file_with_content.readline()
-file_with_content.close()
 
-empty_file = open("new_file.txt", "w")
-empty_file.writelines(a_wali_lines)
-empty_file.close()
+# file_with_content = open("myfile.txt", "r+")
+# line = file_with_content.readline()
+# a_wali_lines = []
+# while line:
+#     location = file_with_content.tell()
+#     if "a" in line or "A" in line:
+#         file_with_content.seek(0)
+#         file_with_content.write("")
+#         a_wali_lines.append(line)
+#     line = file_with_content.readline()
+# file_with_content.close()
+#
+# empty_file = open("new_file.txt", "w")
+# empty_file.writelines(a_wali_lines)
+# empty_file.close()
 
+
+f = open("myfile.txt", "r+")
+q = open("new_file.txt", "w")
+s = f.readline()
+while s:
+    if "a" in s.lower():
+        q.write(s)
+    s = f.readline()
+f.close()
+q.close()
