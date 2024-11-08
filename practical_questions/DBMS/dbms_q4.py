523d957da78ae103e9ec30f00690bb1ee9c59221
Razod <mortonrainy0178@gmail.com>
2024-11-08 12:22:54 +0530

    update

---
+++ practical_questions/DBMS/dbms_q4.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="0178",
+    database="school"
+)
+
+if connection.is_connected():
+    print("Connected to MySQL Database")
+
+    cursor = connection.cursor()
+
+    create_table_query = '''
+    CREATE TABLE students (
+        student_id INT PRIMARY KEY,
+        name VARCHAR(50),
+        age INT,
+        grade VARCHAR(10)
+    );
+    '''
+
+    try:
+        cursor.execute(create_table_query)
+        print("Table 'students' created successfully in the database.")
+    except mysql.connector.Error:
+        print("Error")
+
+    cursor.close()
+    connection.close()
+    print("MySQL connection is closed.")
+
+else:
+    print("Failed to connect to the database.")
